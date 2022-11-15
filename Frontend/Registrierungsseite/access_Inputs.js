e90dddb591f1874aff40431a8f9c5f630c43e1d0
Konwoh <88547137+Konwoh@users.noreply.github.com>
2022-11-15 15:32:06 +0100

    acces User Inputs Email/PW

---
+++ Frontend/Registrierungsseite/access_Inputs.js
@@ -0,0 +1,20 @@
+function access_Data() {
+    var email = document.getElementById("email").value;
+    var passwort = document.getElementById("passwort").value;
+    var passwort_repeat = document.getElementById("pw_repeat").value;
+    var data = {
+        "email": email,
+        "passwort": passwort,
+        "pw_repeat": passwort_repeat
+    }
+    console.log(data)
+    return data
+}
+
+/*function compare_pw() {
+    access_Data()
+    if (access_Data()["passwort"] == access_Data()["pw_repeat"]) {
+        var x = "gut"
+        document.getElementById("demo").innerHTML = x
+    }
+}*/
