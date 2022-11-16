5b7286e31b7e10239282408d8af1cf7527aa678b
Johannes-Factotum <johannes.boldt2@student.uni-halle.de>
2022-11-16 13:56:24 +0100

    Helprequests read and write JSON with List of potential matches

--- backend/HelpRequest.py
+++ backend/HelpRequest.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-import Utilities
+#import Utilities
 
 class HelpRequest():
     id = 0
@@ -14,14 +14,28 @@ class HelpRequest():
     endtime = datetime(1900,1,1)
     potential_matches = []
 
-    def __init__(self, category, plz, deadline,time,description, po = [], i=id()):
-        self.id = i
+    def __init__(self, id,username, category, plz, deadline,description ,starttime, endtime, po = []):
+        self.id = id
+        self.username = username
         self.category = category
         self.plz = plz
         self.deadline = deadline
         self.description = description
-        self.time = time
+        self.startingtime = starttime
+        self.endtime = endtime
         self.potential_matches = po
+    
+    def to_json(self):
+        return {"ID":self.id,
+            "Username":self.username,
+            "subcategory":self.category, 
+            "category":self.category, 
+            "deadline":self.deadline,
+            "PLZ":self.plz,
+            "time_start":self.startingtime,
+            "time_end":self.endtime,
+            "description":self.description,
+            "potential_matches": ",".join(self.potential_matches)}
         
     def setMatch(self,helper_username):
         self.match = helper_username
@@ -31,8 +45,8 @@ class HelpRequest():
     def addPotentialMatch(self, helper_username):
         self.potential_matches.append(helper_username)
                 
-    def loadPotentialMatches(self):
-        list = Utilities.getHelpRequestslist()
-        for l in list:
-            if l.id == self.id:
-                self.potential_matches = l.potential_matches
+    # def loadPotentialMatches(self):
+    #     list = Utilities.getHelpRequestslist()
+    #     for l in list:
+    #         if l.id == self.id:
+    #             self.potential_matches = l.potential_matches
