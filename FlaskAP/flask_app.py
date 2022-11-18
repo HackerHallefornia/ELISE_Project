b634bdd6a2614c90bc63ab068d3609babb205b8b
Ertz012 <91731732+Ertz012@users.noreply.github.com>
2022-11-18 15:12:17 +0100

    import fix

--- FlaskAP/flask_app.py
+++ FlaskAP/flask_app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, jsonify,redirect, url_for, session
-from backend import ListHandling
+import backend.ListHandling as ListHandling
 app = Flask(__name__)
 
 app.secret_key = "JohannesGoebelIstBlöd"
